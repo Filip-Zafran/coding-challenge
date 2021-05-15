e09953f158b9ffe7f196c7fee8ccd7ede432355f
FichoZee <filip.zafran@gmail.com>
2021-05-15 15:05:21 +0200

    fetch product data

--- src/Pages/Components/Dashboard.js
+++ src/Pages/Components/Dashboard.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import Sort from './Sort';
 import ProductCard from './ProductCard';
 
 export default function Dashboard() {
+	const [ products, setProducts ] = useState([]);
+	useEffect(() => {
+		fetch('https://truckoo-backend-aqkoiog6bq-ew.a.run.app/rest/v1/offers/active-offers')
+			.then((res) => res.json())
+			.then((res) => setProducts(res));
+	}, []);
+
 	return (
 		<div>
 			<header className="header">
@@ -11,9 +18,7 @@ export default function Dashboard() {
 				<Sort />
 			</header>
 
-			<div className="result-area">
-				<ProductCard />
-			</div>
+			<div className="result-area">{products.map((product) => <ProductCard product={product} />)}</div>
 		</div>
 	);
 }
