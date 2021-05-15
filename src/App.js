fec5f9562ca89a3dcbec827d1685ee890d64155f
FichoZee <filip.zafran@gmail.com>
2021-05-15 13:53:14 +0200

    create markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<aside className="sidebar">
+				<a>DASHBOARD</a>
+				<a>LOG OUT</a>
+			</aside>
+
+			<div>
+				<header className="header">
+					<div className="search">
+						<input type="text" />
+						<button> Search </button>
+					</div>
+					<div className="filter">
+						<p>Sort</p>
+						{/* <div className="dropdown">
+							<input class="dropdown-content" />
+						</div> */}
+					</div>
+				</header>
+
+				<div className="result-area">INPUT CARD</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
